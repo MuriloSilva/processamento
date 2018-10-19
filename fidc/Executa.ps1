bde379e0008f0ba812d9a209ff230e31929f58ee
Leonardo de Carvalho Botelho <lbotelho@uolinc.com>
2018-10-19 15:50:32 -0300

    Nomenclatura da pasta do FIDC

---
+++ fidc/Executa.ps1
@@ -0,0 +1,68 @@
+Using Module '..\.\Util.psm1';
+
+function ExecutaPrograma {
+    param (
+        $processFile,
+        $processArgs
+    )
+    Push-Location -Path $caminhoAbsolutoDiretorioExecucao;
+
+    MostraLog $caminhoAbsolutoArquivoLog 'INICIANDO...';
+
+    $caminhoAbsolutoArquivoLogStdOut = $caminhoAbsolutoArquivoLog + '.stdout_' + (Get-Date | ForEach-Object {$_.ToString('yyyyMMddHHmmssfff')}) + '.txt';
+    $caminhoAbsolutoArquivoLogStdErr = $caminhoAbsolutoArquivoLog + '.stderr_' + (Get-Date | ForEach-Object {$_.ToString('yyyyMMddHHmmssfff')}) + '.txt';
+
+    Start-Process -File $processFile -ArgumentList $processArgs -RedirectStandardOutput $caminhoAbsolutoArquivoLogStdOut -RedirectStandardError $caminhoAbsolutoArquivoLogStdErr -Wait -NoNewWindow;
+
+    MostraLog $caminhoAbsolutoArquivoLog 'StandardOutput --------------------------------------------------'
+    foreach ($linha in Get-Content $caminhoAbsolutoArquivoLogStdOut) {MostraLog $caminhoAbsolutoArquivoLog $linha $true};
+    MostraLog $caminhoAbsolutoArquivoLog 'StandardError ---------------------------------------------------'
+    foreach ($linha in Get-Content $caminhoAbsolutoArquivoLogStdErr) {MostraLog $caminhoAbsolutoArquivoLog $linha $true};
+
+    MostraLog $caminhoAbsolutoArquivoLog '...TERMINOU';
+
+    Pop-Location;
+}
+
+function Principal {
+    Set-Variable -Name jsonProperties -Value (PegaProperties $MyInvocation.ScriptName) -Option Constant;
+    if ($null -eq $jsonProperties) {
+        MostraLog '' 'Arquivo ".properties" nao localizado.';
+        return;
+    }
+
+    $caminhoAbsolutoArquivoLogTemp = BuscaValorParametro $jsonProperties 'caminhoAbsolutoDiretorioLog';
+    if (Test-Path $caminhoAbsolutoArquivoLogTemp) {
+        $caminhoAbsolutoArquivoLogTemp += ((Get-Item -Path $MyInvocation.ScriptName | ForEach-Object {$_.Name}) -replace '.ps1','') + '_log_';
+        $caminhoAbsolutoArquivoLogTemp += Get-Date | ForEach-Object {$_.ToString('yyyyMMdd')};
+        $caminhoAbsolutoArquivoLogTemp += '.txt'
+    }
+    Set-Variable -Name caminhoAbsolutoArquivoLog -Value $caminhoAbsolutoArquivoLogTemp -Option Constant;
+    Remove-Variable caminhoAbsolutoArquivoLogTemp -Force;
+
+    Set-Variable -Name caminhoAbsolutoDiretorioExecucao -Value (BuscaValorParametro $jsonProperties 'caminhoAbsolutoDiretorioExecucao') -Option Constant;
+    Set-Variable -Name caminhoAbsolutoArquivoJre -Value (BuscaValorParametro $jsonProperties 'caminhoAbsolutoArquivoJre') -Option Constant;
+    Set-Variable -Name parametrosJre -Value (BuscaValorParametro $jsonProperties 'parametrosJre') -Option Constant;
+
+    $podeMandarVer = $true;
+
+    if (!(Test-Path -Path ($caminhoAbsolutoDiretorioExecucao))) {
+        MostraLog $caminhoAbsolutoArquivoLog ('caminhoAbsolutoDiretorioExecucao invalido (' + $caminhoAbsolutoDiretorioExecucao + ').');
+        $podeMandarVer = $false;
+    }
+
+    if (!(Test-Path -Path ($caminhoAbsolutoArquivoJre))) {
+        MostraLog $caminhoAbsolutoArquivoLog ('caminhoAbsolutoArquivoJre invalido (' + $caminhoAbsolutoArquivoJre + ').');
+        $podeMandarVer = $false;
+    }
+
+    if (!$podeMandarVer) {
+        return;
+    }
+
+    ExecutaPrograma $caminhoAbsolutoArquivoJre @($parametrosJre);
+
+    Principal;
+}
+
+Principal;
